d71b79f1699ab6da0da86cd6ad59b604e0b9658b
petross3530mx <45533380+petross3530mx@users.noreply.github.com>
2020-05-04 18:45:14 +0300

    sorry for this part

--- views/header.php
+++ views/header.php
@@ -2,9 +2,7 @@
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Todo Board</title>
-      <!-- Scripts -->
-      <script ksrc="http://october.s-host.net/js/app.js" defer=""></script>
+      <title>Todo Board</title> 
       <!-- Fonts -->
       <link rel="dns-prefetch" href="//fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
